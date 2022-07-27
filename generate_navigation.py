eaa57de3d0fb778efdcfa6d4e75a9ccacf228a3e
Sebastian Hegardt <sehe@netcompany.com>
2022-07-27 15:47:46 +0200

    Implemented Python script to generate navigation

--- generate_navigation.py
+++ generate_navigation.py
@@ -1,23 +1,50 @@
 import os
+import re
+import yaml
+
+REG_FRONT_MATTER = re.compile(r"---(.+)---", re.DOTALL)
 
 
+def get_params_from_front_matter(filepath: str) -> dict:
+    with open(filepath, "r", encoding="utf-8") as f:
+        data = f.read()
+        match = REG_FRONT_MATTER.search(data)
+        front_matter = match.group(1)
+        return yaml.safe_load(front_matter)
 
-def _do_build_navtree(root_path: str, navs: list):
-    for entry in os.scandir(root_path):
-        if entry.is_dir():
-            category = dict()
-            category["title"] = entry.name
+def get_filename_without_extension(filename: str) -> str:
+    return filename.rsplit(".", maxsplit=1)[0]
 
 
-def build_navtree(root_path: str) -> list:
-    navs = list()
-    _do_build_navtree(root_path, navs)
+def get_recipe_navigations(root_path: str) -> dict:
+    categories = []
 
+    for rootDir, subDirs, files in os.walk(root_path, topdown=True):
+        for subDir in subDirs:
+            cat = {
+                "title": subDir.capitalize(),
+                "children": []
+            }
+
+            subDirPath = os.path.join(rootDir, subDir)
+            for subRootDir, subsubDirs, subFiles in os.walk(subDirPath):
+                for subFile in subFiles:
+                    filepath = os.path.join(subDirPath, subFile)
+                    fm_params = get_params_from_front_matter(filepath)
+                    filename = get_filename_without_extension(subFile)
+
+                    cat["children"].append({
+                        "title": fm_params["title"],
+                        "url": "/recipes/{}/{}".format(subDir, filename)
+                    })
+
+            categories.append(cat)
+
+    return {"recipes": categories}
 
 
 if __name__ == '__main__':
-    tree = build_navtree("_recipes/")
+    navs = get_recipe_navigations("_recipes")
 
-
-
-
+    with open("_data/navigation.yml", "w", encoding="utf-8") as f:
+        yaml.dump(navs, f)
--- index.md
+++ index.md
@@ -5,5 +5,4 @@
 layout: index
 ---
 
-
-
+Welcome to a wonderful collection of recipes I've found amazing, over which I would shed many tears if they were lost! Therefore I made this static HTML website to save them forever :)
